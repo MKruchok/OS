187c2342d2596743fddabb30342c1ee404ada21e
Phlake <marko6462@gmail.com>
2022-09-05 22:13:30 +0300

    minor improvements

--- wait3.c
+++ wait3.c
@@ -6,17 +6,20 @@
 
 int main()
 {
-    printf("Parent process (P0), pid = %d\n", getpid());
     pid_t child1, child2;
     child1 = fork();
     if (child1 == 0) {
         printf("My pid = %d (P1), my parent = %d\n", getpid(), getppid());
+        return 0;
     }
     else {
         child2 = fork();
         if (child2 == 0){
             printf("My pid = %d (P2), my parent = %d\n", getpid(), getppid());
+            return 0;
         }
         wait(NULL);
+        wait(NULL);
+        printf("Parent process (P0), pid = %d\n", getpid());
     }
 }
